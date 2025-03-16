582ee9d3b9ab83297810420c71efcab2b4b454ea
Yannik Firre <3316758+YannikFirre@users.noreply.github.com>
2025-03-16 11:04:15 +0100

    Update NavBar.tsx

--- src/app/(main)/NavBar.tsx
+++ src/app/(main)/NavBar.tsx
@@ -97,10 +97,7 @@ export function NavBar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
-        <Icon size="lg">
-          <Icons.Logo />
-        </Icon>
-        <Text>umami</Text>
+
       </div>
       <div className={styles.links}>
         {links.map(({ url, label }) => {
